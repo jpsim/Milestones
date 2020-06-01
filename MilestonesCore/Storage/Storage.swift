47df6091c997530cafa8ce63a29efd7da5ee8f45
JP Simard <jp@jpsim.com>
2020-06-01 09:30:12 -0700

    Migrate data from previous storage locations

--- MilestonesCore/Storage/Storage.swift
+++ MilestonesCore/Storage/Storage.swift
@@ -5,10 +5,14 @@ enum Storage {
         case noAppGroupContainerDirectory
     }
 
-    static let appGroupContainerDirectory = FileManager.default
+    private static let documentDirectory = FileManager.default
+        .urls(for: .documentDirectory, in: .userDomainMask)
+        .first
+
+    private static let appGroupContainerDirectory = FileManager.default
         .containerURL(forSecurityApplicationGroupIdentifier: "group.com.jpsim")
 
-    static let fileName = "milestones.plist"
+    private static let fileName = "milestones.plist"
 
     static func persist(dates: [Milestone]) throws {
         guard let containerDirectory = self.appGroupContainerDirectory else {
@@ -25,8 +29,22 @@ enum Storage {
             throw Error.noAppGroupContainerDirectory
         }
 
+        // Previous versions stored the file in the document directory, so if no file exists in the app group container,
+        // try loading from the document directory to migrate data.
+
         let fileURL = containerDirectory.appendingPathComponent(fileName)
-        let data = try Data(contentsOf: fileURL)
+        let documentFileURL = documentDirectory?.appendingPathComponent(fileName)
+
+        let data: Data
+        if !FileManager.default.fileExists(atPath: fileURL.path),
+            let documentFileURL = documentFileURL,
+            FileManager.default.fileExists(atPath: documentFileURL.path)
+        {
+            data = try Data(contentsOf: documentFileURL)
+        } else {
+            data = try Data(contentsOf: fileURL)
+        }
+
         return try PropertyListDecoder().decode([Milestone].self, from: data)
     }
 }
