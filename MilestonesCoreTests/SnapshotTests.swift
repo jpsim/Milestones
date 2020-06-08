bddf2ab36e5a09ee0082e5b5f6d7b2105d885d13
JP Simard <jp@jpsim.com>
2020-06-07 21:05:24 -0700

    Set TimeZone

--- MilestonesCoreTests/SnapshotTests.swift
+++ MilestonesCoreTests/SnapshotTests.swift
@@ -7,7 +7,8 @@ import XCTest
 
 class SnapshotTests: XCTestCase {
     func testRawData() {
-        let calendar = Calendar(identifier: .gregorian)
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.timeZone = TimeZone(secondsFromGMT: 0)!
         let date = Date(timeIntervalSince1970: 60 * 60 * 24 * 7)
         let dateFormatter = DateFormatter(calendar: calendar, dateStyle: .long)
         XCTAssertEqual(dateFormatter.string(from: date), "January 7, 1970")
