69a2f5afbe02ced3afa1f3a7557f17aa8ea461f8
JP Simard <jp@jpsim.com>
2020-06-07 21:15:27 -0700

    Debug why dates are different

--- MilestonesCoreTests/SnapshotTests.swift
+++ MilestonesCoreTests/SnapshotTests.swift
@@ -9,6 +9,7 @@ class SnapshotTests: XCTestCase {
     func testRawData() {
         var calendar = Calendar(identifier: .gregorian)
         calendar.timeZone = TimeZone(secondsFromGMT: 0)!
+        assertSnapshot(matching: calendar, as: .dump)
         let date = Date(timeIntervalSince1970: 60 * 60 * 24 * 7)
         let dateFormatter = DateFormatter(calendar: calendar, dateStyle: .long)
         XCTAssertEqual(dateFormatter.string(from: date), "January 7, 1970")
