639ab46203f950817a32970032bbc73ed6ee0bf7
JP Simard <jp@jpsim.com>
2020-06-09 21:20:04 -0700

    Hopefully tests are fixed now

--- MilestonesCore/Extensions/DateFormatter+Milestones.swift
+++ MilestonesCore/Extensions/DateFormatter+Milestones.swift
@@ -4,6 +4,8 @@ extension DateFormatter {
     convenience init(calendar: Calendar, dateStyle: DateFormatter.Style = .full, dateFormat: String? = nil) {
         self.init()
         self.calendar = calendar
+        self.timeZone = calendar.timeZone
+        self.locale = calendar.locale
         self.dateStyle = dateStyle
         if let dateFormat = dateFormat {
             self.dateFormat = dateFormat
--- MilestonesCoreTests/SnapshotTests.swift
+++ MilestonesCoreTests/SnapshotTests.swift
@@ -6,40 +6,129 @@ import UIKit
 import XCTest
 
 class SnapshotTests: XCTestCase {
-    func testDateFormatter() {
-        var calendar = Calendar(identifier: .gregorian)
-        calendar.timeZone = TimeZone(secondsFromGMT: 0)!
-        let date = Date(timeIntervalSince1970: 60 * 60 * 24 * 7)
-        let dateFormatter = DateFormatter()
-        dateFormatter.calendar = calendar
-        dateFormatter.dateStyle = .long
-        XCTAssertEqual(dateFormatter.string(from: date), "January 7, 1970")
+    func testCalendarView() {
+        let view = CalendarView(
+            calendar: .testCalendar,
+            startDate: Date(timeIntervalSinceReferenceDate: 60 * 60 * 24 * 8),
+            endDate: Date(timeIntervalSinceReferenceDate: 60 * 60 * 24 * 32)
+        )
+
+        let vc = UIHostingController(rootView: view)
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .light)), named: "light-mode")
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .dark)), named: "dark-mode")
     }
 
-    func test_dateStyleLong() {
+    func testAppView() {
+        let milestone = Milestone(
+            id: UUID(uuidString: "DEADBEEF-DEAD-BEEF-DEAD-BEEFDEADBEEF")!,
+            calendar: .testCalendar,
+            title: "Big Day",
+            today: Date(timeIntervalSinceReferenceDate: 0),
+            date: Date(timeIntervalSinceReferenceDate: 60 * 60 * 24 * 7),
+            isEditing: false
+        )
 
-        let timestamps = [
-            -31536000 : "January 1, 1969 at 12:00:00 AM GMT" , 0.0 : "January 1, 1970 at 12:00:00 AM GMT", 31536000 : "January 1, 1971 at 12:00:00 AM GMT",
-            2145916800 : "January 1, 2038 at 12:00:00 AM GMT", 1456272000 : "February 24, 2016 at 12:00:00 AM GMT", 1456358399 : "February 24, 2016 at 11:59:59 PM GMT",
-            1452574638 : "January 12, 2016 at 4:57:18 AM GMT", 1455685038 : "February 17, 2016 at 4:57:18 AM GMT", 1458622638 : "March 22, 2016 at 4:57:18 AM GMT",
-            1459745838 : "April 4, 2016 at 4:57:18 AM GMT", 1462597038 : "May 7, 2016 at 4:57:18 AM GMT", 1465534638 : "June 10, 2016 at 4:57:18 AM GMT",
-            1469854638 : "July 30, 2016 at 4:57:18 AM GMT", 1470718638 : "August 9, 2016 at 4:57:18 AM GMT", 1473915438 : "September 15, 2016 at 4:57:18 AM GMT",
-            1477285038 : "October 24, 2016 at 4:57:18 AM GMT", 1478062638 : "November 2, 2016 at 4:57:18 AM GMT", 1482641838 : "December 25, 2016 at 4:57:18 AM GMT"
-        ]
+        let store = Store(
+            initialState: AppState(milestones: [milestone]),
+            reducer: appReducer,
+            environment: AppEnvironment(
+                uuid: { fatalError() },
+                persist: { _ in fatalError()},
+                startOfDay: { Date(timeIntervalSinceReferenceDate: 0) },
+                calendar: Calendar(identifier: .gregorian),
+                mainQueue: DispatchQueue.testScheduler.eraseToAnyScheduler(),
+                persistenceQueue: DispatchQueue.testScheduler.eraseToAnyScheduler()
+            )
+        )
+        let view = AppView(store: store)
+        let vc = UIHostingController(rootView: view)
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .light)), named: "light-mode")
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .dark)), named: "dark-mode")
+    }
 
-        let f = DateFormatter()
-        f.dateStyle = .long
-        f.timeStyle = .long
-        f.timeZone = TimeZone(identifier: "GMT")
-        f.locale = Locale(identifier: "en_US_POSIX")
+    func testAppViewEditing() {
+        let milestone = Milestone(
+            id: UUID(uuidString: "DEADBEEF-DEAD-BEEF-DEAD-BEEFDEADBEEF")!,
+            calendar: .testCalendar,
+            title: "Big Day",
+            today: Date(timeIntervalSinceReferenceDate: 0),
+            date: Date(timeIntervalSinceReferenceDate: 60 * 60 * 24 * 7),
+            isEditing: false
+        )
 
-        for (timestamp, stringResult) in timestamps {
+        let store = Store(
+            initialState: AppState(milestones: [milestone], editMode: .active),
+            reducer: appReducer,
+            environment: AppEnvironment(
+                uuid: { fatalError() },
+                persist: { _ in fatalError()},
+                startOfDay: { Date(timeIntervalSinceReferenceDate: 0) },
+                calendar: Calendar(identifier: .gregorian),
+                mainQueue: DispatchQueue.testScheduler.eraseToAnyScheduler(),
+                persistenceQueue: DispatchQueue.testScheduler.eraseToAnyScheduler()
+            )
+        )
+        let view = AppView(store: store)
+        let vc = UIHostingController(rootView: view)
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .light)), named: "light-mode")
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .dark)), named: "dark-mode")
+    }
 
-            let testDate = Date(timeIntervalSince1970: timestamp)
-            let sf = f.string(from: testDate)
+    func testTodayView() {
+        let milestone = Milestone(
+            id: UUID(uuidString: "DEADBEEF-DEAD-BEEF-DEAD-BEEFDEADBEEF")!,
+            calendar: .testCalendar,
+            title: "Big Day",
+            today: Date(timeIntervalSinceReferenceDate: 0),
+            date: Date(timeIntervalSinceReferenceDate: 60 * 60 * 24 * 7),
+            isEditing: false
+        )
 
-            XCTAssertEqual(sf, stringResult)
-        }
+        let store = Store(
+            initialState: AppState(milestones: [milestone]),
+            reducer: appReducer,
+            environment: AppEnvironment(
+                uuid: { fatalError() },
+                persist: { _ in fatalError()},
+                startOfDay: { Date(timeIntervalSinceReferenceDate: 0) },
+                calendar: Calendar(identifier: .gregorian),
+                mainQueue: DispatchQueue.testScheduler.eraseToAnyScheduler(),
+                persistenceQueue: DispatchQueue.testScheduler.eraseToAnyScheduler()
+            )
+        )
+        let view = TodayView(store: store)
+        let vc = UIHostingController(rootView: view)
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .light)), named: "light-mode")
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .dark)), named: "dark-mode")
+    }
 
+    func testMilestoneEditView() {
+        let milestone = Milestone(
+            id: UUID(uuidString: "DEADBEEF-DEAD-BEEF-DEAD-BEEFDEADBEEF")!,
+            calendar: .testCalendar,
+            title: "Big Day",
+            today: Date(timeIntervalSinceReferenceDate: 0),
+            date: Date(timeIntervalSinceReferenceDate: 60 * 60 * 24 * 7),
+            isEditing: false
+        )
+
+        let store = Store(
+            initialState: milestone,
+            reducer: milestoneReducer,
+            environment: MilestoneEnvironment()
+        )
+        let view = MilestoneEditView(store: store)
+        let vc = UIHostingController(rootView: view)
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .light)), named: "light-mode")
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .dark)), named: "dark-mode")
+    }
+}
+
+private extension Calendar {
+    static var testCalendar: Calendar {
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.timeZone = TimeZone(secondsFromGMT: 0)!
+        calendar.locale = Locale(identifier: "en_US_POSIX")
+        return calendar
     }
 }
